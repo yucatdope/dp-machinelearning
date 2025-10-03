4f47600d4b93f3a1d242a6d8971e3ff64dbe96f6
yucatdope <151053753+yucatdope@users.noreply.github.com>
2025-10-03 16:35:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,8 @@ with st.sidebar:
           'gender': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=0)
-
-input_penguins
+  
+input_df
+#input_penguins
 
                       
