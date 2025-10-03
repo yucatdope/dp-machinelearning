1ae345711955a66fb9fdbf6b7e45a9db89f0dea2
yucatdope <151053753+yucatdope@users.noreply.github.com>
2025-10-03 16:26:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,4 +29,7 @@ with st.sidebar:
   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgerson'))
   gender = st.selectbox('Gender', ('male', 'female'))
   bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill Depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body Mass (g)', 2700.0, 6300.0, 4207.0)
                       
