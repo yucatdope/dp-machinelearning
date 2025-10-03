be2b4248af8d05d02fdfa83e90e465d2bf0726ee
yucatdope <151053753+yucatdope@users.noreply.github.com>
2025-10-03 16:33:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ with st.sidebar:
           'bill_depth_mm': bill_depth_mm, 
           'flipper_length_mm': flipper_length_mm,
           'body_mass_g': body_mass_g,
-          'gender':: gender}
+          'gender': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=0)
 
