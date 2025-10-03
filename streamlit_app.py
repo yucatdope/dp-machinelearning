a25f2cd1d9629c63239aadce80d551c5f4982e9d
yucatdope <151053753+yucatdope@users.noreply.github.com>
2025-10-03 16:33:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,11 +33,15 @@ with st.sidebar:
   gender = st.selectbox('Gender', ('male', 'female'))
 
   # Create a Dataframe for the input features
-  data = {'island', island,
-          'bill_length_mm', bill_length_mm,
-          'bill_depth_mm', bill_depth_mm, 
-          'flipper_length_mm', flipper_length_mm,
-          'body_mass_g', body_mass_g,
-          'gender', gender}
+  data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm, 
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'gender':: gender}
   input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X], axis=0)
+
+input_penguins
+
                       
