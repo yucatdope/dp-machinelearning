77835ce53d9f1fca56109f1b49f22ac5f660f771
yucatdope <151053753+yucatdope@users.noreply.github.com>
2025-10-03 16:08:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('Machine Learning App')
 
 st.info('This is app builds a machine learning model')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
